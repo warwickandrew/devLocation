a655f1e155c5922f6db05c9c69507dad95e6fd3d
Andrew Warwick <andrew.warwick@learningpool.com>
2019-05-16 16:18:56 +0100

    basic setup

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import thunk from "redux-thunk";
+// import {createStore, applyMiddleware} from "redux";
+// import {reducer} from "./reducers/flightreducer";
+// import {Provider} from "react-redux";
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<App />, document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
